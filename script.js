12d456b3dfc23866a64e7d6b14fe8dac803913ba
jonathandiaz <jonathan.diaz714@gmail.com>
2021-03-08 04:54:59 -0500

    refactured collision

--- script.js
+++ script.js
@@ -324,13 +324,9 @@ function animate(){
 }
 animate();
 
-function collision(first, second) {
-    if (    !(  first.x > second.x + second.width ||
-                first.x + first.width < second.x ||
-                first.y > second.y + second.height ||
-                first.y + first.height < second.y
-            ) 
-    ) {
+function collision(obj1, obj2) {
+    if (!(obj1.x > obj2.x + obj2.width || obj1.x + obj1.width < obj2.x ||
+        obj1.y > obj2.y + obj2.height || obj1.y + obj1.height < obj2.y)) {
         return true;
     };
 };
