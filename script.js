ad08f626b3bb08201935c3ced16ca82c1c17be0b
jonathandiaz <jonathan.diaz714@gmail.com>
2021-05-19 01:13:39 -0400

    modal close is functional

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var modalBtn = document.querySelector('.modal-button');
 var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
 
 modalBtn.addEventListener('click', function() {
     modalBg.classList.add('bg-active');
+})
+modalClose.addEventListener('click', function() {
+    modalBg.classList.remove('bg-active');
 })
 
 const canvas = document.getElementById('canvas1');
